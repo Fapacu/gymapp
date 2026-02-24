27c9412b10352848ce7fc006b06a25704552c66c
FabriPaniagua <fabriciopaniaguaacuna@gmail.com>
2026-02-24 11:18:57 -0300

    remove create_all and local run

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WTF_CSRF_ENABLED = True
 
     if not SQLALCHEMY_DATABASE_URI:
         raise RuntimeError("DATABASE_URL no está configurada")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
